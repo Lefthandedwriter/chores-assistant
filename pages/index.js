6eedcea92078f35cb7874dfdf1492cced10841ff
Lefthandedwriter <danny@lefthandedwriter.com>
2024-10-23 09:09:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
+import ChoresAssistant from '../components/ChoresAssistant'
 
+function HomePage() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-100">
+      <ChoresAssistant />
+    </main>
+  )
+}
+
+export default HomePage
