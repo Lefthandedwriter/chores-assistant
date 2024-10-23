39d0613d4e9eda094c7094fa01db31a7e5281803
Lefthandedwriter <danny@lefthandedwriter.com>
2024-10-23 09:08:13 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-// File: pages/index.js
-import ChoresAssistant from '@/components/ChoresAssistant'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-8 bg-gray-100">
-      <ChoresAssistant />
-    </main>
-  )
-}
+export default MyApp
